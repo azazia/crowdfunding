95231834713ca7f63b13769e87b923e89241776c
azazia <azaziahmad9@gmail.com>
2022-02-23 10:04:32 +0800

    Get campaign detail: repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
+	FindByCampaignID(ID int) (Campaign, error)
 }
 
 type repository struct {
@@ -32,6 +33,17 @@ func (r *repository) FindByUserID(userID int) ([]Campaign, error){
 	var campaign []Campaign
 
 	err := r.db.Where("user_id = ?", userID).Preload("CampaignImages", "campaign_images.is_primary = true").Find(&campaign).Error
+	if err != nil{
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) FindByCampaignID(ID int) (Campaign, error){
+	var campaign Campaign
+
+	err := r.db.Where("id = ?", ID).Preload("User").Preload("CampaignImages").Find(&campaign).Error
 	if err != nil{
 		return campaign, err
 	}
