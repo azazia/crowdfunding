bfe62a8e8d5677a291183b38e4eef967ce5e180a
azazia <azaziahmad9@gmail.com>
2022-04-07 10:08:44 +0800

    campaign transaction endpoint: service & handler

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,7 +1,46 @@
 package handler
 
+import (
+	"net/http"
+	"website-crowdfunding/helper"
+	"website-crowdfunding/transaction"
+
+	"github.com/gin-gonic/gin"
+)
+
 // parameter di uri
 // tangkap parameter mapping input struct
 // panggil service, parameter input struct
 // service, panggil repo dengan campaign ID
 // repo mencari data transaction suatu campaign
+
+type transactionHandler struct {
+	transactionService transaction.Service
+}
+
+func NewTransactionHandler(transactionService transaction.Service) *transactionHandler{
+	return &transactionHandler{transactionService}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context){
+	var input transaction.GetCampaignTransactionsInput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.transactionService.GetTransactionsByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("failed to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("campaign's transactions", http.StatusOK, "success", transactions)
+	c.JSON(http.StatusOK, response)
+	return
+	
+}
---
+++ transaction/repository_test.go
@@ -0,0 +1,22 @@
+package transaction
+
+import (
+	"log"
+	"testing"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func TestCampaignTransactionRepository(t *testing.T) {
+	dsn := "host=localhost user=postgres password=nciruuxz dbname=crowdfunding port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	campaignTransaction := NewRepository(db)
+
+	campaignTransaction.GetCampaignByID(1)
+}
